751681fb8dd49e316e8784a909c1bd12dcd75214
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-27 18:35:38 +0000

    automatically backed up by learn

---
+++ app/models/superhero.rb
@@ -0,0 +1,16 @@
+class Superhero
+
+  attr_accessor :name, :power, :bio
+  @@all = []
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
