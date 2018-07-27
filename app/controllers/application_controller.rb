f1ad52334aac0722e79a3068defc5db14fb9e189
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-27 19:01:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
 
     post "/teams" do
       @team = Team.new(params[:team])
-      params[:team][:hero].each do |details|
+      params[:team][:superhero].each do |details|
       Hero.new(details)
       end
       @heroes = Hero.all
