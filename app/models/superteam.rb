82a49ea056b5c493c5547f2b862a2ad09e14f876
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-27 18:38:02 +0000

    automatically backed up by learn

--- app/models/superteam.rb
+++ app/models/superteam.rb
@@ -4,8 +4,7 @@ class Superteam
   @@all = []
   def initialize(args)
     @name = args[:name]
-    @power = args[:power]
-    @bio = args[:bio]
+    @motto = args[:motto]
     @@all << self
   end
 
